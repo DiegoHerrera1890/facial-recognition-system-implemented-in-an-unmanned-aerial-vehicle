7c9345a4ca39f52755df5e33f9c6ca901bf5d737
DiegoHerrera1890 <45737011+DiegoHerrera1890@users.noreply.github.com>
2021-02-01 16:15:01 +0900

    changing names of scripts

--- soka_drone/Scripts/Main_node.py
+++ soka_drone/Scripts/Main_node.py
@@ -1,44 +1,118 @@
 #!/usr/bin/env python
-"""
-Created on Fri Jul  3 23:31:20 2020
-Publisher and Subscriber
-Thi is node is for sending data to the drone robot
-according to the XYZ coordinates
-@author: Diego Herrera
-"""
+'''
+This code is to control the position of the drone in the Z axis
+
+Author: @Diego Herrera
+email: alberto18_90@outlook.com
+'''
 
 import rospy
-from std_msgs.msg import String
-from time import sleep
-from command_separator import Command_Parameters
+import mavros
+from geometry_msgs.msg import PoseStamped
+from mavros_msgs.msg import State
+from mavros_msgs.srv import CommandBool, SetMode
+from geometry_msgs.msg import Point
 
 
-def soka_drone(string_to_send):
-    pub.publish(str(string_to_send))
-    rospy.loginfo('Soka_drone: %s', string_to_send)
-    sleep(1)
-    rate.sleep()
+# Callback method for state subscriber
+current_state = State()  # Reading the current state from mavros msgs
+offb_set_mode = SetMode  # Reading the setmode and saving at off_set_mode
 
 
-def reading_csv():
-    """
-    Publishes commands contained in Command_Code_Parameters to slave Raspberry Pi
-    Command_Code_Parameters format = [Haive ID, Hardware key, Command ID, (command relevant parameters)]
-    """
+def state_callback(state):
+    global current_state
+    current_state = state
 
-    for code_parameter in Command_Parameters:
-        # check HAIVE ID is 4001 or 4002  (HAIVE ID will not be included in the data packet that gets published)
-        soka_drone(code_parameter[0:])  # (Hardware key, Command ID, (command relevant parameters))
+
+mavros.set_namespace()
+local_position_publisher = rospy.Publisher(mavros.get_topic('setpoint_position', 'local'), PoseStamped,
+                                           queue_size=10)  #
+state_subscriber = rospy.Subscriber(mavros.get_topic('state'), State, state_callback)
+
+arming_client = rospy.ServiceProxy(mavros.get_topic('cmd', 'arming'), CommandBool)
+takingoff_client = rospy.ServiceProxy(mavros.get_topic('cmd', 'takeoff'), CommandBool)
+landing_client = rospy.ServiceProxy(mavros.get_topic('cmd', 'land'), CommandBool)
+set_mode_client = rospy.ServiceProxy(mavros.get_topic('set_mode'), SetMode)
+
+pose = PoseStamped()
+'''
+pose.pose.position.x = 0
+pose.pose.position.y = 0
+pose.pose.position.z = 2
+'''
+
+
+def puto(data):
+    print("Callback function")
+
+    X = data.x
+    Y = data.y
+    Z = data.z
+    print("X value: ", X)
+    print("Y value: ", Y)
+    print("Z value: ", Z)
+    print("")
+    # pose = PoseStamped()
+    pose.pose.position.x = X
+    pose.pose.position.y = Y
+    pose.pose.position.z = Z
+    # pose.header.stamp = rospy.Time.now()
+    # local_position_publisher.publish(pose)
+    '''
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+    print("ruso huevo loco")
+    '''
+
+
+def position_control():
+    print("Position control def")
+    rospy.init_node('Offboard_node', anonymous=True)
+    prev_state = current_state
+    rate = rospy.Rate(20.0)
+
+    # Sending a few points before start
+    for i in range(100):
+        local_position_publisher.publish(pose)
+        rate.sleep()
+
+    # We need to wait for FCU connection
+    while not current_state.connected:
+        rate.sleep()
+
+    last_request = rospy.get_rostime()
+
+    while not rospy.is_shutdown():
+        print("While not rospy.is_shutdown")
+        now = rospy.get_rostime()
+        if current_state.mode != "OFFBOARD" and (now - last_request > rospy.Duration(5.)):
+            set_mode_client(base_mode=0, custom_mode="OFFBOARD")
+            last_request = now
+
+        else:
+            if not current_state.armed and (now - last_request > rospy.Duration(5.)):
+                arming_client(True)
+                last_request = now
+
+        if current_state.armed:
+            print("El drone esta listo pa volar homie")
+        if prev_state.armed != current_state.armed:
+            rospy.loginfo("Vehicle armed: %r" % current_state.armed)
+        if prev_state.mode != current_state.mode:
+            rospy.loginfo("Current mode: %s" % current_state.mode)
+
+        prev_state = current_state
+        print("Going to callback function")
+
+        rospy.Subscriber("SOKA_DRONE", Point, puto)
+        print("after callback function")
+        pose.header.stamp = rospy.Time.now()
+        print("Local_position_publisher____ publishing coordinates")
+        local_position_publisher.publish(pose)
+        rate.sleep()
 
 
 if __name__ == '__main__':
-    rospy.init_node('Main_node', anonymous=True)
-    pub = rospy.Publisher('Quadcopter_coordinates', String, queue_size=10)
-    sleep(1)
-    print("Main node ready")
-    rate = rospy.Rate(10)
     try:
-        reading_csv()
-        print("Done")
+        position_control()
     except rospy.ROSInterruptException:
         pass
