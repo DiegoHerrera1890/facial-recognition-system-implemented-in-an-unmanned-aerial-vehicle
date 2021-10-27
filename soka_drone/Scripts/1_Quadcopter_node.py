d88e183a329a433b37d7b19c6f3fee5e063c7dcd
DiegoHerrera1890 <45737011+DiegoHerrera1890@users.noreply.github.com>
2021-10-27 13:00:53 +0900

    Add files via upload

--- soka_drone/Scripts/1_Quadcopter_node.py
+++ soka_drone/Scripts/1_Quadcopter_node.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-'''
+"""
 This code is to control the position of the drone in the Z axis
 Author: @Diego Herrera
 email: alberto18_90@outlook.com
+"""
+
 '''
 import cv2
 import rospy
@@ -13,18 +15,21 @@ from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Point
 from geometry_msgs.msg import Pose
-#from gazebo_msgs.msg import ModelStates
+# from gazebo_msgs.msg import ModelStates
 from tf.transformations import quaternion_from_euler
 from time import sleep
 
 # Callback method for state subscriber
 current_state = State()  # Reading the current state from mavros msgs
 
+
 def state_callback(state):
     global current_state
     current_state = state
 
+
 mavros.set_namespace()
+
 
 def coordinates_orientation_2(data_2):
     # print('data of Face Found: ', data_2)
@@ -38,9 +43,9 @@ def coordinates_orientation_2(data_2):
     pose.pose.orientation.z = data_2.orientation.z  # quat[2]
     pose.pose.orientation.w = data_2.orientation.w  # quat[3]
 
+
 def land_callback(msg):
     global flag
-    print("perrrrrrooooo")
     flag = True
 
 
@@ -106,16 +111,118 @@ if __name__ == '__main__':
     try:
         rospy.init_node('Quadcopter_node', anonymous=True)
         local_position_publisher = rospy.Publisher(mavros.get_topic('setpoint_position', 'local'), PoseStamped,
-                                           queue_size=10)  #
+                                                   queue_size=10)  #
         state_subscriber = rospy.Subscriber(mavros.get_topic('state'), State, state_callback)
         arming_client = rospy.ServiceProxy(mavros.get_topic('cmd', 'arming'), CommandBool)
         takeoff_client = rospy.ServiceProxy(mavros.get_topic('cmd', 'takeoff'), CommandTOL)
         landing_client = rospy.ServiceProxy(mavros.get_topic('cmd', 'land'), CommandTOL)
         set_mode_client = rospy.ServiceProxy(mavros.get_topic('set_mode'), SetMode)
         pose = PoseStamped()
-        #global flag
+        # global flag
         flag = False
         position_control()
     except rospy.ROSInterruptException:
-        
+
+        pass
+'''
+import rospy
+import mavros
+from mavros_msgs.msg import State
+from mavros_msgs.srv import CommandBool, CommandTOL, SetMode
+from std_msgs.msg import String
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose
+# from gazebo_msgs.msg import ModelStates
+from time import sleep
+
+current_state = State()  # Reading the current state from mavros msgs
+
+mavros.set_namespace()
+
+
+class drone_control:
+
+    def state_callback(self, state):
+        self.current_state = state
+
+    def coordinates_orientation_2(self, data_2):
+        # print('data of Face Found: ', data_2)
+        self.pose.header.stamp = rospy.Time.now()
+        self.pose.pose.position.x = data_2.position.x
+        self.pose.pose.position.y = data_2.position.y
+        self.pose.pose.position.z = data_2.position.z
+        # quat = quaternion_from_euler(rVal2, pVal2, yawVal2) # + pi_2)
+        self.pose.pose.orientation.x = data_2.orientation.x  # quat[0]
+        self.pose.pose.orientation.y = data_2.orientation.y  # quat[1]
+        self.pose.pose.orientation.z = data_2.orientation.z  # quat[2]
+        self.pose.pose.orientation.w = data_2.orientation.w  # quat[3]
+
+    def land_callback(self, msg):
+        self.flag = True
+
+    def __init__(self):
+        self.flag = False
+        self.pose = PoseStamped()
+        self.local_position_publisher = rospy.Publisher(mavros.get_topic('setpoint_position', 'local'), PoseStamped,
+                                                        queue_size=10)  #
+        self.sub = rospy.Subscriber("/Face_recognition/local_position", Pose, self.coordinates_orientation_2)
+        self.state_subscriber = rospy.Subscriber(mavros.get_topic('state'), State, self.state_callback)
+        self.arming_client = rospy.ServiceProxy(mavros.get_topic('cmd', 'arming'), CommandBool)
+        self.takeoff_client = rospy.ServiceProxy(mavros.get_topic('cmd', 'takeoff'), CommandTOL)
+        # landing_client = rospy.ServiceProxy(mavros.get_topic('cmd', 'land'), CommandTOL)
+        self.set_mode_client = rospy.ServiceProxy(mavros.get_topic('set_mode'), SetMode)
+        #self.current_state = ''
+        print("Position control function")
+        self.prev_state = self.current_state
+        rate = rospy.Rate(20.0)
+
+        for i in range(100):
+            self.local_position_publisher.publish(self.pose)
+            rate.sleep()
+
+        while not self.current_state.connected:
+            rate.sleep()
+
+        self.last_request = rospy.get_rostime()
+
+        while not rospy.is_shutdown():
+            now = rospy.get_rostime()
+            # qxVal, yVal, zVal, rVal, pVal, yawVal = 0 , 0, 1.6, 0, 0, 0
+            if current_state.mode != "OFFBOARD" and (now - self.last_request > rospy.Duration(5.)):
+                self.set_mode_client(base_mode=0, custom_mode="OFFBOARD")
+                last_request = now
+
+            else:
+                if not self.current_state.armed and (now - self.last_request > rospy.Duration(5.)):
+                    self.arming_client(True)
+                    last_request = now
+
+            if self.current_state.armed:
+                print("Drone ready to fly")
+            if self.prev_state.armed != current_state.armed:
+                rospy.loginfo("Vehicle armed: %r" % current_state.armed)
+            if self.prev_state.mode != current_state.mode:
+                rospy.loginfo("Current mode: %s" % current_state.mode)
+
+            self.prev_state = current_state
+
+            # rospy.Subscriber("/Face_recognition/drone_search", Pose, coordinates_orientation)
+            self.pose.header.stamp = rospy.Time.now()
+            self.local_position_publisher.publish(self.pose)
+            rospy.Subscriber("/Face_recognition/landing", String, self.land_callback)
+            if self.flag:
+                print("hello landing")
+                self.set_mode_client(base_mode=0, custom_mode="AUTO.LAND")
+                sleep(8)
+                self.flag = False
+                break
+
+            rate.sleep()
+
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('Quadcopter_node', anonymous=True)
+        drone_control()
+    except rospy.ROSInterruptException:
         pass
